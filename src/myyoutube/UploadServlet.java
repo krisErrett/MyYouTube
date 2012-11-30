c4e5b8ffbeb9b04cba751cd57746ca7945ca35b7
rshepherd <me@rshepherd.org>
2012-11-30 16:36:58 -0500

    listing files on s3 and uploading done

--- src/myyoutube/UploadServlet.java
+++ src/myyoutube/UploadServlet.java
@@ -13,18 +13,42 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.IOUtils;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 @SuppressWarnings("serial")
 public class UploadServlet extends HttpServlet
 {
+    private static final String AWS_S3_BUCKET = "randy-1354310562177";
+    private static final String AWS_ACCESS_KEY = "AKIAIYA4NVASTTYAM4QA";
+    private static final String AWS_SECRET_KEY = "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";
+    
+    private static final AWSCredentials CREDS = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY);
+    private static final AmazonS3 S3 = new AmazonS3Client(CREDS);
+    
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException
     {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Upload!");
+        resp.setContentType("text/html");
+        ObjectListing objects = S3.listObjects(AWS_S3_BUCKET);
+        do {
+                for (S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
+                    resp.getWriter().println(objectSummary.getKey() + "<br>");    
+                }
+                objects = S3.listNextBatchOfObjects(objects);
+        } while (objects.isTruncated());
     }
     
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+    {
         try {
             @SuppressWarnings("unchecked")
             List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(req);
@@ -41,16 +65,27 @@ public class UploadServlet extends HttpServlet
                     // Process form file field (input type="file").
                     String fieldname = item.getFieldName();
                     String filename = item.getName();
-                    InputStream filecontent = item.getInputStream();
-                    // ... (do your job here)
+                    
+                    // Figure out content length for S3 header
+                    InputStream cis = item.getInputStream();
+                    byte[] contentBytes = IOUtils.toByteArray(cis);
+                    Long contentLength = Long.valueOf(contentBytes.length);
+                    ObjectMetadata metadata = new ObjectMetadata();
+                    metadata.setContentLength(contentLength);
+
+                    PutObjectResult result = S3.putObject (
+                            AWS_S3_BUCKET, filename, 
+                            item.getInputStream(), metadata
+                    );
+                    
                     resp.getWriter().println("fieldname="+fieldname);
-                    resp.getWriter().println("fieldvalue="+filename);
+                    resp.getWriter().println("file="+filename);
+                    resp.getWriter().println("result="+result.getETag());
                 }
             }
         } catch (FileUploadException e) {
             throw new ServletException("Cannot parse multipart request.", e);
         }
-
-        // ...
+        
     }
 }
